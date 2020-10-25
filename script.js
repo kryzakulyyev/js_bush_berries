cfbb846f08e54e0d3c45aaad8d953e79f433ddb4
KASYMJAN RYZAKULYYEV <macpro2020@MacBook-Pro.local>
2020-10-24 23:17:30 -0700

    Added files and js script

---
+++ script.js
@@ -0,0 +1,27 @@
+/*
+  The space travellers have safely landed and are foraging for food in the natural wildlife.
+
+  There are bushes with many different colour berries.
+  The pink berries are the ONLY safe ones to eat.
+  If any other berries are present, it's best not to eat from the bush at all!
+
+  Create a function which checks if the bush has ALL PINK berries and is safe for the astronauts to eat from the bush.
+
+  Use the tests to confirm which message to return
+*/
+
+let bushBerryColours1 = ["pink", "pink", "pink", "neon", "pink", "transparent"]
+let bushBerryColours2 = ["pink", "pink", "pink", "pink"] 
+
+//For Loop method
+findPink=arr=>{
+  let count=0;
+  for(let i=0; i<arr.length; i++){
+    if(arr[i]==="pink"){
+      count++; 
+    } 
+  }
+  return count === arr.length? "Safe to eat":"Not safe to eat"
+} 
+   console.log(findPink(bushBerryColours1))
+   console.log(findPink(bushBerryColours2))
